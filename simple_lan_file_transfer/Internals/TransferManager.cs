7f72d7674213e82c6221e7920441c187777ddacf
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-08-30 00:05:53 +0200

    Add message Header struct to TransferManager

--- simple_lan_file_transfer/Internals/TransferManager.cs
+++ simple_lan_file_transfer/Internals/TransferManager.cs
@@ -12,6 +12,39 @@ public class ReceiverTransferManager : TransferManagerBase
 
 public abstract class TransferManagerBase
 {
+   protected struct Header
+   {
+      public enum MessageType
+      {
+         FileName,
+         FileHash,
+         FileBlock,
+         LastBlockReadResponse
+      }
+
+      public const int Size = 9;
+      
+      public MessageType Type { get; init; }
+      public ulong DataSize { get; init; }
+      
+      public byte[] ToBytes()
+      {
+         var bytes = new byte[Size];
+         bytes[0] = (byte)Type;
+         BitConverter.TryWriteBytes(bytes.AsSpan(1, 8), DataSize);
+         return bytes;
+      }
+      
+      public static Header FromBytes(byte[] bytes)
+      {
+         return new Header
+         {
+            Type = (MessageType)bytes[0],
+            DataSize = BitConverter.ToUInt64(bytes.AsSpan(1, 8))
+         };
+      }
+   }
+      
    protected readonly Socket Socket;
    public TransferManagerBase(Socket socket)
    {
