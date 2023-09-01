f634bf4114602d0508b028ac51e87c47717a8853
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-09-01 18:06:49 +0200

    Finish first version of ParentingConnectionManager class

--- simple_lan_file_transfer/Internals/ParentingConnectionManager.cs
+++ simple_lan_file_transfer/Internals/ParentingConnectionManager.cs
@@ -1,10 +1,21 @@
 namespace simple_lan_file_transfer.Internals;
 
-public abstract class ParentingConnectionManager<TChild> : IDisposable where TChild : IDisposable
+using System.Collections.Concurrent;
+
+public abstract class ParentingConnectionManager<TChild> : IDisposable where TChild : IDisposable 
 {
     protected bool Disposed;
     
     private RequestListener _requestListener;
+    private readonly ConcurrentBag<TChild> _children = new();
+    
+    protected ParentingConnectionManager(int port)
+    {
+        _requestListener = new RequestListener(port, HandleNewConnectionRequestAsync);
+    }
+
+    protected abstract Task HandleNewConnectionRequestAsync(Socket socket, CancellationToken cancellationToken);
+    protected abstract Task<TChild> StartNewConnectionAsync(IPAddress ipAddress, int port, CancellationToken cancellationToken);
     
     public void Dispose()
     {
@@ -20,16 +31,18 @@ public abstract class ParentingConnectionManager<TChild> : IDisposable where TCh
         
         if (disposing)
         {
+            _requestListener.Dispose();
             DisposeOfAllChildren();
-            
         }
         
         Disposed = true;
     }
-    
+
     private void DisposeOfAllChildren()
     {
-        _requestListener?.Dispose();
-        _requestListener = null;
+        foreach (TChild child in _children)
+        {
+            child.Dispose();
+        }
     }
 }
