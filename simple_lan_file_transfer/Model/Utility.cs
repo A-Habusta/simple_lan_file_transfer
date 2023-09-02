4cd398160788a709a6b48d12db26fa77c2b630bd
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-09-02 05:22:15 +0200

    Change default block size

--- simple_lan_file_transfer/Model/Utility.cs
+++ simple_lan_file_transfer/Model/Utility.cs
@@ -2,7 +2,7 @@ namespace simple_lan_file_transfer.Internals;
 
 internal static class Utility
 {
-    public const int BlockSizeKb = 8;
+    public const int BlockSizeKb = 32;
     public const int BlockSize = BlockSizeKb * 1024; 
     
     public const ushort DefaultPort = 52123;
