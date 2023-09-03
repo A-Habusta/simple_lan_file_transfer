75931de6e217991451fbdc6a5ad24d059b825dc6
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-09-03 21:55:53 +0200

    Update missing usings

--- simple_lan_file_transfer/Model/Usings.cs
+++ simple_lan_file_transfer/Model/Usings.cs
@@ -1,7 +1,9 @@
 global using System;
 global using System.IO;
 global using System.Linq;
+global using System.ComponentModel;
 global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
 
 global using System.Net;
 global using System.Net.Sockets;
@@ -9,4 +11,3 @@ global using System.Net.NetworkInformation;
 
 global using System.Threading;
 global using System.Threading.Tasks;
-
