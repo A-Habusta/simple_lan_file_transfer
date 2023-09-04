608e52aa79037e53101a1c981ad249365082c67e
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-09-04 23:48:32 +0200

    Add new empty views

---
+++ simple_lan_file_transfer/Views/SingleConnectionTabView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace simple_lan_file_transfer.Views;
+
+public partial class SingleConnectionTabView : UserControl
+{
+    public SingleConnectionTabView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ simple_lan_file_transfer/Views/TransferView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace simple_lan_file_transfer.Views;
+
+public partial class TransferView : UserControl
+{
+    public TransferView()
+    {
+        InitializeComponent();
+    }
+}
