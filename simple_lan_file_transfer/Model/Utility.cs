949c52cd16bfcc73258ca4a54cf5a542e5c057d7
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-09-06 01:35:19 +0200

    Add methods to handle byte suffixes

--- simple_lan_file_transfer/Model/Utility.cs
+++ simple_lan_file_transfer/Model/Utility.cs
@@ -11,4 +11,54 @@ internal static class Utility
     public const int BroadcastIntervalMs = 2000;
 
     public const string DefaultMetadataDirectory = ".transfers_in_progress";
+
+
+    public enum ByteSuffix
+    {
+        B = 0,
+        KB,
+        MB,
+        GB,
+        TB,
+        PB,
+        EB,
+    }
+
+    public static string AddHighestPossibleByteSuffixToNumber(long number)
+    {
+
+        ByteSuffix suffix = GetHighestPossibleByteSuffixForNumber(number);
+        return AddByteSuffixToNumber(number, suffix);
+    }
+
+    public static string AddByteSuffixToNumber(long number, ByteSuffix suffix)
+        => $"{DivideNumberToFitSuffix(number, suffix):F2} {suffix}";
+
+    public static ByteSuffix GetHighestPossibleByteSuffixForNumber(long number)
+    {
+        const long divisor = 1024;
+
+        var suffix = ByteSuffix.B;
+        while (number > divisor)
+        {
+            number /= divisor;
+            ++suffix;
+        }
+
+        return suffix;
+    }
+
+    public static double DivideNumberToFitSuffix(long number, ByteSuffix suffix)
+    {
+        const double divisor = 1024;
+
+        var numberFloatingPoint = (double)number;
+        for (var i = 0; i < (int)suffix; ++i)
+        {
+            numberFloatingPoint /= divisor;
+        }
+
+        return numberFloatingPoint;
+    }
+
 }
