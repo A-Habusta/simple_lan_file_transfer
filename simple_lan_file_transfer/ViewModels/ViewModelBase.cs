a94a4b1fc7eea6ee97daa75d55abb44fb518b0b7
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-09-07 00:49:56 +0200

    Add ShowPopup method to shared ViewModelBase

--- simple_lan_file_transfer/ViewModels/ViewModelBase.cs
+++ simple_lan_file_transfer/ViewModels/ViewModelBase.cs
@@ -1,7 +1,24 @@
 ﻿using ReactiveUI;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Base;
+using MsBox.Avalonia.Enums;
 
 namespace simple_lan_file_transfer.ViewModels;
 
 public class ViewModelBase : ReactiveObject
 {
+    // No need to use dispatcher, the internals of messagebox already do that
+    protected static async Task ShowPopup(string message)
+    {
+        var messageBox = MessageBoxManager.GetMessageBoxStandard(
+            title: "Error",
+            text: message,
+            ButtonEnum.OkCancel);
+        await ShowPopup(messageBox);
+    }
+
+    protected static async Task<ButtonResult> ShowPopup(IMsBox<ButtonResult> messageBox)
+    {
+        return await messageBox.ShowAsync();
+    }
 }
