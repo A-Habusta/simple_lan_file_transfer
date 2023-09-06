8aec654fab83bd775d7fa3ae30c27f2cf684b903
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-09-06 07:04:26 +0200

    Add method to access text boxes

--- simple_lan_file_transfer/Views/MainView.axaml.cs
+++ simple_lan_file_transfer/Views/MainView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using simple_lan_file_transfer.ViewModels;
 
 namespace simple_lan_file_transfer.Views;
 
@@ -7,5 +9,14 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    public void StartNewTransfer(object? sender, RoutedEventArgs e)
+    {
+        var ip = TextBoxTargetIp.Text ?? string.Empty;
+        var password = TextBoxOutboundPassword.Text ?? string.Empty;
+
+
+        (DataContext as MainViewModel)?.StartFileSendAsync(ip, password);
     }
 }
